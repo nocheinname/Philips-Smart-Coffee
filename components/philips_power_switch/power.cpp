c333fb2ceb07d3e39545366b86786a604731373a
nocheinname <45895516+nocheinname@users.noreply.github.com>
2023-10-19 20:42:38 +0200

    Update power.cpp

--- components/philips_power_switch/power.cpp
+++ components/philips_power_switch/power.cpp
@@ -24,7 +24,7 @@ namespace esphome
                 {
                     // Send pre-power on message
                     for (unsigned int i = 0; i <= MESSAGE_REPETITIONS; i++)
-                        mainboard_uart_->write_array({0xD5, 0x55, 0x01, 0x01, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x35, 0x05});
+                        mainboard_uart_->write_array({0xD5, 0x55, 0x0A, 0x01, 0x02, 0x00, 0x02, 0x00, 0x00, 0x00, 0x0E, 0x12});
                    
 
                     // Send power on message
