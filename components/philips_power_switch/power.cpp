88f14ed88f59a69418d4b80c4d7827fdf16f892b
nocheinname <45895516+nocheinname@users.noreply.github.com>
2023-10-19 20:17:41 +0200

    Update power.cpp

--- components/philips_power_switch/power.cpp
+++ components/philips_power_switch/power.cpp
@@ -26,7 +26,7 @@ namespace esphome
                     for (unsigned int i = 0; i <= MESSAGE_REPETITIONS; i++)
                         mainboard_uart_->write_array({0xD5, 0x55, 0x00, 0x01, 0x02, 0x00, 0x02, 0x08, 0x00, 0x00, 0x39, 0x1C});
                     delay(600)
-                        for (unsigned int i = 0; i <= MESSAGE_REPETITIONS; i++)
+                        for (unsigned int i = 0; i <= MESSAGE_REPETITIONS_LONG; i++)
                         mainboard_uart_->write_array({0xD5, 0x55, 0x0A, 0x01, 0x02, 0x00, 0x02, 0x00, 0x00, 0x00, 0x0E, 0x12});
 
                     // Send power on message
